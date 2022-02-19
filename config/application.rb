c092166678508f36f9066fd85ee1c760798d8946
César Intriago <cesar.int@outlook.com>
2022-02-19 10:39:44 -0500

    Set up create JWT token

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module DebitsAndCreditsApi
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.api_only = true
     config.eager_load_paths << Rails.root.join('lib')
+    config.api_only = true
   end
 end
