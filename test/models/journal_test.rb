a08870604db9061a01c44b33710490a8dcc8a82d
César Intriago <cesar.int@outlook.com>
2022-02-20 15:38:04 -0500

    Test journal model

--- test/models/journal_test.rb
+++ test/models/journal_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class JournalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create journal' do
+    journal = Journal.new(description: 'new journal')  
+    assert_not journal.valid?
+  end
 end
