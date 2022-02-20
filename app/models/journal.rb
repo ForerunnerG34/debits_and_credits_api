38fcc599a4cf65ed16dc81b1f5793549b746f8a6
César Intriago <cesar.int@outlook.com>
2022-02-20 16:01:11 -0500

    Add validations to Journals

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,4 +1,11 @@
 class Journal < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
+  validates_presence_of :user
+  validates :transactions, length: {
+    minimum: 1,
+    maximum: 2,
+    message: 'A Journal can only have 1 or 2 transactions'
+  }
+
 end
--- test/models/journal_test.rb
+++ test/models/journal_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 class JournalTest < ActiveSupport::TestCase
-  test 'should create journal' do
-    journal = Journal.new(description: 'new journal')  
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should create journal with one' do
+    transaction = Transaction.new(amount: 9.99, user: @user)
+    journal = Journal.new(description: 'new journal', user: @user, transactions: [transaction])
+    assert journal.valid?
+  end
+
+  test 'should not create journal with no transactions' do
+    journal = Journal.new(description: 'new journal', user: @user, transactions: [])
     assert_not journal.valid?
   end
+
+  test 'should not create journal with more than two transactions' do
+    transaction_one = Transaction.new(amount: 9.99, user: @user)
+    transaction_two = Transaction.new(amount: 9.99, user: @user)
+    transaction_three = Transaction.new(amount: 9.99, user: @user)
+    journal = Journal.new(description: 'new journal', user: @user, transactions: [transaction_one, transaction_two, transaction_three])
+
+    assert_not journal.valid?
+  end
+
 end
